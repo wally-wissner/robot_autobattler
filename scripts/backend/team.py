1b6290e491ed295a111734c8b052c02f40e1d5f4
wally-wissner <Wally.Wissner.MS@gmail.com>
2022-11-29 22:15:47 -0500

    Converted team to dataclass.

--- scripts/backend/team.py
+++ scripts/backend/team.py
@@ -1,23 +1,14 @@
+from dataclasses import dataclass
+from typing import List
+
 from scripts.backend.inventory import Inventory
+from scripts.backend.unit import Unit
+from scripts.backend.upgrades import UnitUpgrade
 
 
+@dataclass
 class Team(object):
-    def __init__(self, ai=None, currency=0):
-        self.ai = ai
-        self.player = ai is not None
-        self.currency = currency
-        self.units = {}
-        self.inventory_upgrades = Inventory()
-
-    def win(self):
-        # TODO
-        raise NotImplemented()
-
-    def lose(self):
-        # TODO
-        raise NotImplemented()
-
-
-    def end_turn(self):
-        for unit in self.units:
-            unit.on_turn_end()
+    player: bool
+    units: List[Unit]
+    unit_upgrades: Inventory[UnitUpgrade]
+    currency: int = 0
