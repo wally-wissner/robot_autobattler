1d3d7fc90669a1ddae912f8b56c6948f1b80abe2
wally-wissner <Wally.Wissner.MS@gmail.com>
2023-01-19 17:59:47 -0500

    Improved supported cases and fixed bugs.

--- scripts/utilities/game_math.py
+++ scripts/utilities/game_math.py
@@ -1,17 +1,21 @@
-def clamp(x, min_value, max_value):
+def clamp(value, min_value, max_value):
     assert min_value <= max_value, "min_value must be less than or equal to max_value."
-    return sorted([x, min_value, max_value])[1]
+    return sorted([value, min_value, max_value])[1]
 
 
-def move_forward(items, index):
+def move_forward(items: list, index: int) -> None:
     if len(items) == 0:
-        return ValueError("No items found.")
-    if index != 0:
+        raise ValueError("No items found.")
+    try:
         items[index - 1], items[index] = items[index], items[index - 1]
+    except IndexError:
+        pass
 
 
-def move_backward(items, index):
+def move_backward(items: list, index: int) -> None:
     if len(items) == 0:
-        return ValueError("No items found.")
-    if index != len(items - 1):
+        raise ValueError("No items found.")
+    try:
         items[index], items[index + 1] = items[index + 1], items[index]
+    except IndexError:
+        pass
