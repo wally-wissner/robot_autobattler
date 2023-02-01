7ece024e2cc7e7e81afff81fb7f78594e11a2104
wally-wissner <Wally.Wissner.MS@gmail.com>
2023-01-31 22:02:10 -0500

    Update physics.py

--- scripts/backend/physics.py
+++ scripts/backend/physics.py
@@ -1,23 +1,36 @@
+import math
 import pygame
-from shapely.geometry import Polygon
-from dataclasses import dataclass
+from shapely.geometry import Point, Polygon
 
 
-tolerance = 1e-8
+class DiscObject(object):
+    def __init__(
+            self,
+            radius: float,
+            position: pygame.Vector2 = pygame.Vector2(),
+            velocity: pygame.Vector2 = pygame.Vector2(),
+            acceleration: pygame.Vector2 = pygame.Vector2(),
+            resistance: float = 0,
+    ):
+        self._radius = radius
+        self.shape: Polygon = Point().buffer(distance=radius)
+        self.position = position
+        self.velocity = velocity
+        self.acceleration = acceleration
+        self.resistance = resistance
 
+    @property
+    def radius(self):
+        return self._radius
 
-@dataclass
-class RigidBody(object):
-    shape: Polygon
-    position: pygame.Vector2
-    velocity: pygame.Vector2
-    friction: float
+    @radius.setter
+    def radius(self, value):
+        self._radius = value
+        self.shape = Point().buffer(distance=value)
 
     def update(self, dt):
-        self.velocity += (-self.friction * self.velocity.normalize()) * dt
+        self.velocity += (self.acceleration - self.resistance * self.velocity) * dt
         self.position += self.velocity * dt
 
-
-@dataclass
-class KinematicBody(RigidBody):
-    mass: float
+        if math.isclose((self.velocity - pygame.Vector2()).magnitude(), 0, abs_tol=1e-8):
+            self.velocity = pygame.Vector2()
