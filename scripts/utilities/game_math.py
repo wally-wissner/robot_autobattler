35f085a273fcb9df2f1a669b67f13eea612a31f0
wally-wissner <Wally.Wissner.MS@gmail.com>
2023-07-11 17:33:21 -0400

    Added type hints.

--- scripts/utilities/game_math.py
+++ scripts/utilities/game_math.py
@@ -1,5 +1,6 @@
 import math
 from pyglet.math import Vec2
+from typing import Self
 
 
 def clamp(value, min_value, max_value):
@@ -19,6 +20,7 @@ def shift_item(items: list, index: int, shift=1) -> None:
 
 class Vector2(Vec2):
     def __init__(self, *args, **kwargs):
+        self.x, self.y = None, None
         if not args and not kwargs:
             x, y = 0, 0
         elif len(args) == 1:
@@ -29,35 +31,35 @@ class Vector2(Vec2):
             x, y = kwargs["x"], kwargs["y"]
         super().__init__(x, y)
 
-    def as_tuple(self):
+    def as_tuple(self) -> tuple:
         return self.x, self.y
+
+    def magnitude(self) -> float:
+        return math.sqrt(self.x**2 + self.y**2)
 
     def __iter__(self):
         return iter(self.as_tuple())
 
-    def __eq__(self, other):
+    def __eq__(self, other: Self):
         return self.x, self.y == other.x, other.y
 
-    def __add__(self, other):
+    def __add__(self, other: Self):
         return Vector2(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other):
+    def __sub__(self, other: Self):
         return Vector2(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other):
+    def __mul__(self, other: float):
         return Vector2(self.x * other, self.y * other)
 
-    def __rmul__(self, other):
+    def __rmul__(self, other: float):
         return self * other
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: float):
         return Vector2(self.x / other, self.y / other)
 
-    def __round__(self, n=None):
+    def __round__(self, n: int = None):
         return Vector2(round(self.x, n), round(self.y, n))
-
-    def magnitude(self):
-        return math.sqrt(self.x**2 + self.y**2)
 
     def __repr__(self):
         return f"Vector2({self.x}, {self.y})"
