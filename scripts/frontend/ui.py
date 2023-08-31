702072cfde64f68547c93eb9c14d5da3cab7360e
wally-wissner <Wally.Wissner.MS@gmail.com>
2023-08-30 20:25:52 -0400

    Changed text from description to name.

--- scripts/frontend/ui.py
+++ scripts/frontend/ui.py
@@ -43,27 +43,29 @@ class UITextBox(arcade.gui.UITexturePane):
 class UIUnitUpgrade(arcade.gui.UIPadding, arcade.gui.UIDraggableMixin):
     def __init__(self, unit_upgrade: UnitUpgrade, height: int, width: int, x: int, y: int):
         self.unit_upgrade = unit_upgrade
+        self.card_color = colors.RETRO_RED
+        self.badge_color = colors.RETRO_BLUE
         self.texture_card = arcade.texture.Texture(
             name="bg_card",
-            image=Image.new(mode='RGB', size=(width, height // 2), color=colors.RETRO_RED),
+            image=Image.new(mode='RGB', size=(width, height // 2), color=self.card_color),
         )
         self.texture_badge = arcade.texture.Texture(
             name="bg_badge",
-            image=Image.new(mode='RGB', size=(width, height // 2), color=colors.RETRO_BLUE),
+            image=Image.new(mode='RGB', size=(width, height // 2), color=self.badge_color),
         )
         self.box = arcade.gui.UIBoxLayout(x=x, y=y, vertical=True, space_between=0)
         self.box.add(UITextBox(
             width=width,
             height=height//2,
             texture=self.texture_card,
-            text=self.unit_upgrade.card.description(),
+            text=self.unit_upgrade.card.name,
             font_size=10,
         ))
         self.box.add(UITextBox(
             width=width,
             height=height//2,
             texture=self.texture_badge,
-            text=self.unit_upgrade.badge.description(),
+            text=self.unit_upgrade.badge.name,
             font_size=10,
         ))
         super().__init__(child=self.box, bg_color=colors.RARE, height=height, width=width, padding=(2, 2, 2, 2))
