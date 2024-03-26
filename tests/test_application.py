4404ef332945a3c17fa839283b0d7cabbb238278
Wally Wissner <wally.wissner@gmail.com>
2024-03-26 16:35:39 -0400

    Add unittest for running application.

---
+++ tests/test_application.py
@@ -0,0 +1,19 @@
+# pylint: disable=broad-exception-caught
+
+import unittest
+
+from frontend.application import application
+from frontend.scenes import scene_map
+
+
+class TestApplication(unittest.TestCase):
+    def setUp(self):
+        self.application = application
+        self.application.load_scene_map(scene_map)
+
+    def test_quit(self):
+        try:
+            application.run()
+            application.quit()
+        except Exception as e:
+            self.fail(f"Application encountered an error: {e}")
