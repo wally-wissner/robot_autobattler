211e751869137568feb904ed9be363fb37af7069
Wally Wissner <wally.wissner@gmail.com>
2024-05-05 22:13:48 -0400

    Fix path for fonts.

--- src/robot_autobattler/frontend/fonts.py
+++ src/robot_autobattler/frontend/fonts.py
@@ -1,11 +1,10 @@
-# import pygame
-# from functools import cache
-# from os.path import join
-#
-# from config import ROOT_DIRECTORY
-# from utils.enums import EFont
-#
-#
-# @cache
-# def get_font(font: EFont, size: int):
-#     return pygame.font.Font(join(ROOT_DIRECTORY, f"assets/fonts/{font.value}.ttf"), size)
+import pygame
+from functools import cache
+
+from config import absolute_path
+from utils.enums import EFont
+
+
+@cache
+def get_font(font: EFont, size: int):
+    return pygame.font.Font(absolute_path(f"assets/fonts/{font.value}.ttf"), size)
